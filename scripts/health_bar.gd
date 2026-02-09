6a87ed36c862ae76a82cd1a2ca3f09761fe2de0d
Yonaki51 <yonivandon@hotmail.com>
2026-02-09 14:21:45 +0100

    health bar werkend gemaakt met aanraking van de vijand

--- scripts/health_bar.gd
+++ scripts/health_bar.gd
@@ -1,5 +1,9 @@
 extends Sprite2D
 
+func _ready():
+	frame = 8
+	
+
 func update_bar(current_health: int, max_health: int):
 	var pct = float(current_health) / max_health
-	frame = 8 - round(pct * 8)
+	frame = round(pct * 8)
