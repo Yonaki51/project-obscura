0df0e0474aaf5ac38c35cc84002ed1a1046c8894
Yonaki51 <yonivandon@hotmail.com>
2026-02-10 09:35:06 +0100

    wanneer de speler damage neemt veranderd de sprite lichtelijk van kleur

--- scripts/hitbox.gd
+++ scripts/hitbox.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var cooldown: bool = false
+@onready var player: CharacterBody2D = $"../../../player"
 
 func _physics_process(_delta):
 	if cooldown:
@@ -8,9 +9,11 @@ func _physics_process(_delta):
 	var player = get_tree().get_first_node_in_group("player")
 	if player:
 		var dist = global_position.distance_to(player.global_position)
-		if dist < 10 and player.has_method("take_damage"):
+		if dist < 15 and player.has_method("take_damage"):
+
 			cooldown = true
 			player.take_damage(1)
+			player.modulate = Color(1, 0.5, 0.5)
 			
 			if player.current_health <= 0:
 				print("You died!")
@@ -21,5 +24,7 @@ func _physics_process(_delta):
 					get_tree().reload_current_scene()
 				)
 			else:
+				await get_tree().create_timer(0.5).timeout
+				player.modulate = Color(1, 1, 1)
 				await get_tree().create_timer(1.0).timeout
 				cooldown = false
