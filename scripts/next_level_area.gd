8e2f087bd84f7c0a48b9a49a20ad3f9a5f036582
smth1808 <MB.Straub@gmail.com>
2026-02-11 12:03:05 +0100

    Next_level werkt en ik ben geweldig
    Je kunt het spel nu starten met level

--- scripts/next_level_area.gd
+++ scripts/next_level_area.gd
@@ -1,7 +1,10 @@
 extends Area2D
 
-func _on_body_entered(body) -> void:
+func _on_body_entered(body):
 	if body.is_in_group("player"):
+		
 		var current_scene_file = get_tree().current_scene.scene_file_path
-		var next_level_number = current_scene_file.to_int() +1
-		print(next_level_number)
+		var next_level_number = current_scene_file.to_int() + 1
+		
+		var next_level_path = "res://scenes/level_" + str(next_level_number) + ".tscn" 
+		get_tree().change_scene_to_file(next_level_path)
